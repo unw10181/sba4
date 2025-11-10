e165796a44a4839d7cddb2c517b4f3879b0ac9e2
unw10181 <uchennanwilliams@gmail.com>
2025-11-09 22:38:59 -0500

    proper input forms

---
+++ script.js
@@ -0,0 +1,18 @@
+let tasks = [];
+
+let taskName = document.getElementById('taskName');
+let category = document.getElementById('category');
+let deadLine = document.getElementById('taskName');
+let status = document.getElementById('taskName');
+let addTaskBtn = document.getElementById('addtaskBtn');
+
+
+addTaskBtn.addEventListener("click", () => {
+    let task = {
+        
+    }
+})
+
+function createTask() {
+
+}
